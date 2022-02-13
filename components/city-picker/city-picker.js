d1ee6564f4fb5be03dc3afd75132305492a31732
vincentzyc <vincentzyc@qq.com>
2022-02-13 16:02:47 +0800

    🎉 init: init

--- components/city-picker/city-picker.js
+++ components/city-picker/city-picker.js
@@ -35,7 +35,7 @@ Component({
   methods: {
     pickerChange() {
       if (Array.isArray(this.data.multiText)) {
-        this.setData({ multiStr: this.data.fullcity ? value.join(' ') : this.data.multiText[1] })
+        this.setData({ multiStr: this.data.fullcity ? this.data.multiText.join(' ') : this.data.multiText[1] })
         this.triggerEvent('getcity', this.data.multiText)
       }
     },
@@ -43,9 +43,11 @@ Component({
       if (Array.isArray(this.data.multiArr) && this.data.multiArr.length === 0) return []
       const province = this.data.multiArr[0][multiIndex[0]]
       const city = multiIndex[1] == 0 || multiIndex[1] ? this.data.multiArr[1][multiIndex[1]] : ''
+      const area = multiIndex[2] == 0 || multiIndex[2] ? this.data.multiArr[2][multiIndex[2]] : ''
       let multiText = []
       if (province) multiText.push(province)
       if (city) multiText.push(city)
+      if (area) multiText.push(area)
       return multiText
     },
     setMultiArr(defaultCity) {
@@ -54,7 +56,8 @@ Component({
       if (Array.isArray(defaultCity) && defaultCity.length > 0) {
         const province = defaultCity[0] ? defaultCity[0] : ''
         const city = defaultCity[1] ? defaultCity[1] : ''
-        let provinces = [], provinceIndex = 0, citys = [], cityIndex = 0
+        const area = defaultCity[2] ? defaultCity[2] : ''
+        let provinces = [], provinceIndex = 0, citys = [], cityIndex = 0, areas = [], areaIndex = 0
         for (let index1 = 0; index1 < cityInfo.length; index1++) {
           const element1 = cityInfo[index1];
           provinces.push(element1.n)
@@ -70,18 +73,28 @@ Component({
             cityIndex = index2
           }
         }
+        const cityObj = provinceObj.c[cityIndex];
+        console.log(cityObj);
+        for (let index3 = 0; index3 < cityObj.c.length; index3++) {
+          const element3 = cityObj.c[index3];
+          areas.push(element3.n)
+          if (element3.n === area) {
+            areaIndex = index3
+          }
+        }
         this.setData({
-          multiArr: [provinces, citys],
-          multiIndex: [provinceIndex, cityIndex],
-          multiText: [provinces[provinceIndex], citys[cityIndex]]
+          multiArr: [provinces, citys, areas],
+          multiIndex: [provinceIndex, cityIndex, areaIndex],
+          multiText: [provinces[provinceIndex], citys[cityIndex], areas[areaIndex]]
         })
       } else {
         const provinces = cityInfo.map(v => v.n) || []
-        const citys = cityInfo[0].c.map(v => v.n) || []
+        const citys = cityInfo[0].c ? cityInfo[0].c.map(v => v.n) : []
+        const areas = cityInfo[0].c[0].c ? cityInfo[0].c[0].c.map(v => v.n) : []
         this.setData({
-          multiArr: [provinces, citys],
-          multiIndex: [0, 0],
-          multiText: [provinces[0], citys[0]]
+          multiArr: [provinces, citys, areas],
+          multiIndex: [0, 0, 0],
+          multiText: [provinces[0], citys[0], areas[0]]
         })
       }
     },
@@ -92,6 +105,9 @@ Component({
           break;
         case 1:
           this.setMultiArr(this.getMultiText([this.data.multiIndex[0], e.detail.value]))
+          break;
+        case 2:
+          this.setMultiArr(this.getMultiText([this.data.multiIndex[0], this.data.multiIndex[1], e.detail.value]))
           break;
       }
     },
