d9dd1505eae153ab6e906023e01fe7922bf30d6d
vincentzyc <vincentzyc@qq.com>
2022-02-14 12:13:51 +0800

    🎉 init: init

--- pages/form/form.js
+++ pages/form/form.js
@@ -24,7 +24,7 @@ Page({
       name: '《入网许可协议》',
       link: 'https://bilibili.com'
     }],
-    selectNum: { "pid": "无", "num": "13205617333", "item": [{ "numChar": "1", "highlight": false }, { "numChar": "3", "highlight": false }, { "numChar": "2", "highlight": false }, { "numChar": "0", "highlight": false }, { "numChar": "5", "highlight": false }, { "numChar": "6", "highlight": false }, { "numChar": "1", "highlight": false }, { "numChar": "7", "highlight": false }, { "numChar": "3", "highlight": true }, { "numChar": "3", "highlight": true }, { "numChar": "3", "highlight": true }], "id": 66838830, "salePrice": "108", "marketPrice": "1080", "areaInfo": "淮北联通", "chargesStr": "此号码为淮北市联通，129元/月套餐，30G全国高速流量，500分钟全国通话，主叫0.1元/分钟，被叫免费，套餐承诺最低消费199元/月，合约期不低于180个月", "preferential": "享24个月套餐7折优惠，仅限本地上门办理", "prestore": "0", "numProvince": "安徽省", "numCity": "淮北市", "phoneStatus": 1, "phoneStatusStr": "未占用" }
+    selectNum: null
   },
   onLoad: function () {
     const selectNum = app.getGlobal('selectNumber')
@@ -96,6 +96,7 @@ Page({
     this.data.form['templateUrl'] = JSON.stringify(app.getGlobal('query'))
     wx.showLoading({ title: '提交中...' })
     console.log(this.data.form);
+    app.setGlobal('submitForm', this.data.form)
     let res = await Api.Common.addOrder(this.data.form)
     wx.hideLoading()
     console.log(res);
