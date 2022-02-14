736dfb6e9260a504a26c45a61beaba84c335895e
vincentzyc <vincentzyc@qq.com>
2022-02-14 12:13:40 +0800

    🎉 init: init

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ import EventBus from '/event-bus/index.js';
 
 App({
   eventBus: new EventBus(),
+  getUserInfo() {
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId    
+        this.globalData.userCode = res.code
+      }
+    });
+  },
   onLaunch(options) {
-    this.globalData.pid = options.query.pid
+    if (options.query.pid) this.globalData.pid = options.query.pid
     this.globalData.query = options.query
+    this.getUserInfo()
     // // 展示本地存储能力
     // const logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
-
-    // // 登录
-    // wx.login({
-    //   success: res => {
-    //     // {code: "053FumFa1Y7JCC0lCSHa1xbuD11FumFT" ,errMsg: "login:ok"}
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //   }
-    // })
   },
   setGlobal(key, value) {
     this.globalData[key] = value
@@ -26,9 +27,11 @@ App({
     return this.globalData[key] || null
   },
   globalData: {
+    userCode: '',
     pid: '23126',
     query: '',
     cmData: null,
-    selectNumber: null
+    selectNumber: null,
+    submitForm: null
   }
 })
