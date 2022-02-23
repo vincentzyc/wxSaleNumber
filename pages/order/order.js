30e232c434029fdeccaeb1be007329b3e6ef2c67
vincentzyc <vincentzyc@qq.com>
2022-02-23 08:53:40 +0800

    🎉 init: init

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,13 +1,35 @@
 // pages/order.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    inputNum: ''
   },
-
+  onNumInput(e) {
+    this.setData({ inputNum: e.detail.value });
+  },
+  async handleSearch() {
+    if (!this.data.inputNum) {
+      wx.showToast({
+        title: '请输入手机号',
+        icon: 'none',
+        duration: 2000
+      })
+      return false;
+    }
+    if (!/^(?:(?:\+|00)86)?1[3-9]\d{9}$/.test(this.data.inputNum)) {
+      wx.showToast({
+        title: '请输入正确的手机号码',
+        icon: 'none',
+        duration: 2000
+      })
+      return false;
+    }
+    wx.navigateTo({
+      url: '../orderlist/orderlist?phone=' + this.data.inputNum
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
