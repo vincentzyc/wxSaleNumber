892464ae5b49a28d207559421f81599c7cd5f24c
vincentzyc <vincentzyc@qq.com>
2022-02-23 08:53:24 +0800

    🎉 init: init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,47 @@
 // 获取应用实例
+import Api from '../../api/index'
+
 const app = getApp()
 
 Page({
   data: {
-    bannerUrl: ''
+    bannerUrl: '',
+    hiddenBackTop: true
+  },
+  back2top() {
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300
+    })
+  },
+  onPageScroll: function (e) {
+    if (e.scrollTop > 300) {
+      this.setData({
+        hiddenBackTop: false
+      });
+    } else {
+      this.setData({
+        hiddenBackTop: true
+      });
+    }
+  },
+  back2top() {
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300
+    })
+  },
+  async getTemplateInfo() {
+    const templateId = app.getGlobal('templateId')
+    if (templateId) {
+      try {
+        const res = await Api.wxGet(Api.CommonBaseUrl + '/api/promotionPage/' + templateId)
+        const pageData = JSON.parse(res.promotionPageValue)
+        if (pageData && pageData.wxPayAccount) app.setGlobal('wxPayAccount', pageData.wxPayAccount)
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
   onReachBottom() {
     app.eventBus.emit('onReachBottom')
@@ -12,10 +50,19 @@ Page({
     if (url) this.setData({ bannerUrl: url })
   },
   onLoad() {
-    console.log('onLoad');
     app.eventBus.on('onGetBanner', url => this.onGetBanner(url))
+    this.getTemplateInfo()
   },
-  onShow: function () {
-    // this.setData({ oldTab: this.data.active, active: this.data.oldTab });
+  onShareAppMessage() {
+    return {
+      title: '一店联精品号',
+      path: '/pages/index/index?pid=' + app.getGlobal('pid')
+    }
+  },
+  onShareTimeline() {
+    return {
+      title: '一店联精品号',
+      query: 'pid=' + app.getGlobal('pid')
+    }
   }
 })
